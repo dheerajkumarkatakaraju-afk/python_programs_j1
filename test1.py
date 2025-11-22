5c20dcfeea8b76ab9fea1a1f53d980bc36d97d2d
dheerajkumarkatakaraju-afk <dheerajkumarkatakaraju@gmail.com>
2025-11-22 09:45:05 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,5 @@
+print("welcom to profile building")
+a=10
+b=20
+result=a=b
+print("addition of a,b is:",result)
---
+++ variables.py
@@ -0,0 +1,7 @@
+#variables-is a memory allocation to valuename
+name="dheeraj"
+age=26
+salary=200000000.0
+print(name)
+print(age)
+print(salary)
